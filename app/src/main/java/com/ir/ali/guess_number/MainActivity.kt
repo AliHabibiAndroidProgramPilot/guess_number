25e660a6fe2889885ebc68c71de6d1edd4a43abd
Ali <biz.alihabibi@gmail.com>
2024-08-12 12:58:22 +0330

    Matching range text with Radio Group

--- app/src/main/java/com/ir/ali/guess_number/MainActivity.kt
+++ app/src/main/java/com/ir/ali/guess_number/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ir.ali.guess_number
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ir.ali.guess_number.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.oneToFive.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.rangeText.text = "Range is 1..5"
+        }
+        binding.oneToTen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.rangeText.text = "Range is 1..10"
+        }
+        binding.oneToTwenty.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.rangeText.text = "Range is 1..20"
+        }
+    }
+
+    private fun matchTextWithRadioGroup() {
+        binding.rangeRadioGroup.setOnCheckedChangeListener { radioGroup, _ ->
+            when (radioGroup.id) {
+                R.id.one_to_five -> {
+                    binding.rangeText.text = "Range is 1..5"
+                }
+                R.id.one_to_ten -> {
+                    binding.rangeText.text = "Range is 1..10"
+                }
+                R.id.one_to_twenty -> {
+                    binding.rangeText.text = "Range is 1..20"
+                }
+            }
+        }
     }
 }
