6f6ef5b62992db9da4b74449bec89340edc83491
Ali <biz.alihabibi@gmail.com>
2024-08-12 14:31:23 +0330

    configure input boxes

--- app/src/main/java/com/ir/ali/guess_number/MainActivity.kt
+++ app/src/main/java/com/ir/ali/guess_number/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ir.ali.guess_number
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
 import com.ir.ali.guess_number.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestFocus()
         binding.oneToFive.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 val anim = AnimationUtils.loadAnimation(this, R.anim.aplha)
@@ -39,5 +42,11 @@ class MainActivity : AppCompatActivity() {
                 binding.rangeText.text = "Range is 1..20"
             }
         }
+    }
+    private fun requestFocus() {
+        val editText: View = binding.guess1
+        editText.requestFocus()
+        val makeFocus: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        makeFocus.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
